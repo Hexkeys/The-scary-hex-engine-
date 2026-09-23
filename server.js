e85444aefc2be04e66d586ae3d275c8242a86d05
Hexlabs <worapol.pun.797@gmail.com>
2026-09-23 07:52:53 +0700

    Fix Express 5 wildcard route

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 10000;
+
 app.use(express.static(path.join(__dirname)));
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 app.listen(port, '0.0.0.0', () => console.log(`Hex Search listening on ${port}`));
